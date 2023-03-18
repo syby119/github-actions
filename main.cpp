80440d0a79b28f3419dd3371aaa0c483b6c1acf4
syby119 <syby119@126.com>
2023-03-18 21:19:37 +0800

    Test macos

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main() {
-    std::cout << "Github Actions" << std::endl;
+    std::cout << "Github Actions Test" << std::endl;
+
+    std::ifstream ifs("../../main.cpp");
+    if (!ifs.is_open()) {
+        std::cout << "Cannot Open File" << std::endl;
+        return 0;
+    }
+
+    std::cout << "Here" << std::endl;
+
+    for (std::string line; std::getline(ifs, line); ) {
+        std::cout << line << std::endl;
+    }
+
     return 0;
 }
